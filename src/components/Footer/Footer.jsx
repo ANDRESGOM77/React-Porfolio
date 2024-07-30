0f6907e84719fb900d49d73fbcc81565fd7fd278
ANDRESGOM77 <andresgom2009@gmail.com>
2024-07-30 22:45:36 +1000

    adding Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <a href="#" className={styles.up}>
+        
+      </a>
+      <div className={styles.socials}>
+
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
